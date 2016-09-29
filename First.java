d5f89ffdda80d8207a5f214d0b68fb147a1f060e
AGvido <agvido@gmail.com>
2016-09-29 21:05:23 +0400

    догрузил старый проект

---
+++ First.java
@@ -0,0 +1,18 @@
+package javaTask.git;
+
+/**
+ * Created by AGvido on 27.09.2016.
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Привет Дарик");
+        testLab1();
+    }
+    static void testLab1(){
+        System.out.println("Как дела");
+        System.out.println("Ты уверен");
+    }
+    static void testLab2(){
+        System.out.println("а не порали нам пора");
+    }
+}
