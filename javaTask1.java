b08ebd8c58fe3b54390e5b477b7779c4b4154ad1
AGvido <agvido@gmail.com>
2016-09-29 14:36:14 +0300

    Первая выгрузка задачи

---
+++ javaTask1.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Zaytsev on 29.09.2016.
+ */
+public class javaTask1 {
+
+    public static void main(String[] args){
+        System.out.println("Нулевая запись");
+        alexGroup2();
+        alexGroup();
+    }
+    public static void alexGroup(){
+        System.out.println("Начало записи");
+    }
+    static void alexGroup2(){
+        System.out.println("Вторая запись");
+        System.out.println("Третья запись");
+    }
+    static void alexGroup3(){
+        System.out.println("Четвертая запись");
+    }
+}
