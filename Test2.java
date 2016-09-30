5fa13daf1012f83cc4dcd682ca4d78b553e9806f
AGvido <agvido@gmail.com>
2016-09-30 12:11:32 +0300

    Еще один класс для тестирования

---
+++ Test2.java
@@ -0,0 +1,18 @@
+package javaTask.git;
+
+/**
+ * Created by Zaytsev on 30.09.2016.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Еще один класс");
+        System.out.println("Проверяем его");
+    }
+
+    static void testClass2(){
+        System.out.println("testClass2");
+    }
+    static void testClass3() {
+        System.out.println("testClass3");
+    }
+}
